2e770de962ce4fecece2bb8a14f7b566ade93335
Saurabh Patil <119738647+saurabhpatil90@users.noreply.github.com>
2023-06-05 11:27:28 +0530

    Add files via upload

---
+++ Backend/index.js
@@ -0,0 +1,24 @@
+const connectToMongo = require('./db');
+connectToMongo(); 
+const express = require('express')
+var cors = require('cors')
+
+const app = express()
+const port = 5000
+
+
+app.use(cors())
+
+app.use(express.json());
+// available routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!.....saurabh')
+})
+
+app.listen(port, () => {
+  console.log(`iNotebook backend listening on port ${port}`)
+})
